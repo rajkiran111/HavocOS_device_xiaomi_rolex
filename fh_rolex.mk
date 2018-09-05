6589869f390a578e44a339b3afa9ff8146571147
rajkiran111 <rajkiran1230@gmail.com>
2018-09-05 16:16:00 +0530

    Update fh_rolex.mk

--- fh_rolex.mk
+++ fh_rolex.mk
@@ -22,3 +22,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from rolex device
 $(call inherit-product, device/xiaomi/rolex/device.mk)
 
+# Device identifier. This must come after all inclusions
+PRODUCT_BRAND := Xiaomi
+PRODUCT_DEVICE := rolex
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_MODEL := Redmi 4A
+PRODUCT_NAME := fh_rolex
+TARGET_VENDOR_PRODUCT_NAME := rolex
+
