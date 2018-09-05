d71cd46df08368f250680e6637c9cebec1b41a0b
rajkiran111 <rajkiran1230@gmail.com>
2018-09-05 15:52:19 +0530

    Update fh.mk

--- fh.mk
+++ fh.mk
@@ -19,11 +19,15 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common firehound stuff
+# Inherit some common LineageOS stuff
 $(call inherit-product, vendor/fh/config/common_full_phone.mk)
+
+# Boot animation
+TARGET_BOOT_ANIMATION_RES := 720
 
 # Inherit from rolex device
 $(call inherit-product, device/xiaomi/rolex/device.mk)
+
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := rolex
